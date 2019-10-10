b9592f2be070bde9f91428b0ef6f216aa4d6f157
Jakub Jezek <jakub@pype.club>
2019-10-10 15:41:15 +0200

    feat(nks): adding collect metadata plugin

--- pype/plugins/nukestudio/publish/collect_metadata.py
+++ pype/plugins/nukestudio/publish/collect_metadata.py
@@ -4,7 +4,7 @@ from pyblish import api
 class CollectClipMetadata(api.InstancePlugin):
     """Collect Metadata from selected track items."""
 
-    order = api.CollectorOrder + 0.01
+    order = api.CollectorOrder + 0.011
     label = "Collect Metadata"
     hosts = ["nukestudio"]
 
@@ -14,11 +14,13 @@ class CollectClipMetadata(api.InstancePlugin):
         ms_metadata = self.metadata_to_string(
             dict(item.source().mediaSource().metadata()))
 
-        instance.data["clipMetadata"] = ti_metadata
-        instance.data["mediaSourceMetadata"] = ms_metadata
+        instance.data["metadata"] = dict(ms_metadata, **ti_metadata)
 
-        self.log.info(instance.data["clipMetadata"])
-        self.log.info(instance.data["mediaSourceMetadata"])
+        self.log.info("Collected Metadata of `source media` and `track item`")
+        self.log.debug(
+            "Metadata keys: `{}`".format(instance.data["metadata"].keys())
+        )
+
         return
 
     def metadata_to_string(self, metadata):
