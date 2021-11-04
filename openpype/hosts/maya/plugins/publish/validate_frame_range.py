6915b069a25e4a66fa6781e96fc2e151ed690757
Eric <eric@super-dope.tv>
2021-11-03 22:49:29 -0700

    commented out entire plugin to avoid several settings edits

--- openpype/hosts/maya/plugins/publish/validate_frame_range.py
+++ openpype/hosts/maya/plugins/publish/validate_frame_range.py
@@ -1,93 +1,93 @@
-import pyblish.api
-import openpype.api
+# import pyblish.api
+# import openpype.api
 
-from maya import cmds
+# from maya import cmds
 
 
-class ValidateFrameRange(pyblish.api.InstancePlugin):
-    """Valides the frame ranges.
+# class ValidateFrameRange(pyblish.api.InstancePlugin):
+#     """Valides the frame ranges.
 
-    This is optional validator checking if the frame range on instance
-    matches the one of asset. It also validate render frame range of render
-    layers
+#     This is optional validator checking if the frame range on instance
+#     matches the one of asset. It also validate render frame range of render
+#     layers
 
-    Repair action will change everything to match asset.
+#     Repair action will change everything to match asset.
 
-    This can be turned off by artist to allow custom ranges.
-    """
+#     This can be turned off by artist to allow custom ranges.
+#     """
 
-    label = "Validate Frame Range"
-    order = openpype.api.ValidateContentsOrder
-    families = ["animation",
-                "pointcache",
-                "camera",
-                "renderlayer",
-                "review",
-                "yeticache"]
-    optional = True
-    actions = [openpype.api.RepairAction]
+#     label = "Validate Frame Range"
+#     order = openpype.api.ValidateContentsOrder
+#     families = ["animation",
+#                 "pointcache",
+#                 "camera",
+#                 "renderlayer",
+#                 "review",
+#                 "yeticache"]
+#     optional = True
+#     actions = [openpype.api.RepairAction]
 
-    def process(self, instance):
-        context = instance.context
-        if instance.data.get("tileRendering"):
-            self.log.info((
-                "Skipping frame range validation because "
-                "tile rendering is enabled."
-            ))
-            return
+#     def process(self, instance):
+#         context = instance.context
+#         if instance.data.get("tileRendering"):
+#             self.log.info((
+#                 "Skipping frame range validation because "
+#                 "tile rendering is enabled."
+#             ))
+#             return
 
-        frame_start_handle = int(context.data.get("frameStartHandle"))
-        frame_end_handle = int(context.data.get("frameEndHandle"))
-        handles = int(context.data.get("handles"))
-        handle_start = int(context.data.get("handleStart"))
-        handle_end = int(context.data.get("handleEnd"))
-        frame_start = int(context.data.get("frameStart"))
-        frame_end = int(context.data.get("frameEnd"))
+#         frame_start_handle = int(context.data.get("frameStartHandle"))
+#         frame_end_handle = int(context.data.get("frameEndHandle"))
+#         handles = int(context.data.get("handles"))
+#         handle_start = int(context.data.get("handleStart"))
+#         handle_end = int(context.data.get("handleEnd"))
+#         frame_start = int(context.data.get("frameStart"))
+#         frame_end = int(context.data.get("frameEnd"))
 
-        inst_start = int(instance.data.get("frameStartHandle"))
-        inst_end = int(instance.data.get("frameEndHandle"))
+#         inst_start = int(instance.data.get("frameStartHandle"))
+#         inst_end = int(instance.data.get("frameEndHandle"))
 
-        # basic sanity checks
-        assert frame_start_handle <= frame_end_handle, (
-            "start frame is lower then end frame")
+#         # basic sanity checks
+#         assert frame_start_handle <= frame_end_handle, (
+#             "start frame is lower then end frame")
 
-        assert handles >= 0, ("handles cannot have negative values")
+#         assert handles >= 0, ("handles cannot have negative values")
 
-        # compare with data on instance
-        errors = []
+#         # compare with data on instance
+#         errors = []
 
-        if(inst_start != frame_start_handle):
-            errors.append("Instance start frame [ {} ] doesn't "
-                          "match the one set on instance [ {} ]: "
-                          "{}/{}/{}/{} (handle/start/end/handle)".format(
-                              inst_start,
-                              frame_start_handle,
-                              handle_start, frame_start, frame_end, handle_end
-                          ))
+#         if(inst_start != frame_start_handle):
+#             errors.append("Instance start frame [ {} ] doesn't "
+#                           "match the one set on instance [ {} ]: "
+#                           "{}/{}/{}/{} (handle/start/end/handle)".format(
+#                               inst_start,
+#                               frame_start_handle,
+#                               handle_start, frame_start, frame_end, handle_end
+#                           ))
 
-        if(inst_end != frame_end_handle):
-            errors.append("Instance end frame [ {} ] doesn't "
-                          "match the one set on instance [ {} ]: "
-                          "{}/{}/{}/{} (handle/start/end/handle)".format(
-                              inst_end,
-                              frame_end_handle,
-                              handle_start, frame_start, frame_end, handle_end
-                          ))
+#         if(inst_end != frame_end_handle):
+#             errors.append("Instance end frame [ {} ] doesn't "
+#                           "match the one set on instance [ {} ]: "
+#                           "{}/{}/{}/{} (handle/start/end/handle)".format(
+#                               inst_end,
+#                               frame_end_handle,
+#                               handle_start, frame_start, frame_end, handle_end
+#                           ))
 
-        for e in errors:
-            self.log.error(e)
+#         for e in errors:
+#             self.log.error(e)
 
-        assert len(errors) == 0, ("Frame range settings are incorrect")
+#         assert len(errors) == 0, ("Frame range settings are incorrect")
 
-    @classmethod
-    def repair(cls, instance):
-        """
-        Repair instance container to match asset data.
-        """
-        cmds.setAttr(
-            "{}.frameStart".format(instance.data["name"]),
-            instance.context.data.get("frameStartHandle"))
+#     @classmethod
+#     def repair(cls, instance):
+#         """
+#         Repair instance container to match asset data.
+#         """
+#         cmds.setAttr(
+#             "{}.frameStart".format(instance.data["name"]),
+#             instance.context.data.get("frameStartHandle"))
 
-        cmds.setAttr(
-            "{}.frameEnd".format(instance.data["name"]),
-            instance.context.data.get("frameEndHandle"))
+#         cmds.setAttr(
+#             "{}.frameEnd".format(instance.data["name"]),
+#             instance.context.data.get("frameEndHandle"))
